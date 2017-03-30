2875de887c929267dcd7c76286887702ad2793ca
Rob Lacey <contact@robl.me>
2017-03-30 23:02:40 +0100

    builder

--- lib/metatron/builder.rb
+++ lib/metatron/builder.rb
@@ -1,12 +1,24 @@
 module Metatron
   class Builder
-    attr_accessor :title, :description
-
     def assign(hash = {})
       hash.each do |k, v|
         send("#{k}=", v)
       end
       self
+    end
+
+    def method_missing(method, *args)
+      if method[-1] == '=' && args.size == 1
+        return instance_variable_set("@#{method[0..-2]}", args[0])
+      end
+      if args.size == 0
+        if r = instance_variable_get("@#{method}")
+          return "#{r} - #{I18n.t('site_name')}"
+        else
+          return I18n.t("meta.#{method}")
+        end
+      end
+      super
     end
   end
 end
